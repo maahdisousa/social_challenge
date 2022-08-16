3ee9879dc40e84100bce7b5d6e31c12c906341b1
maahdisousa <mmdisousa@gmail.com>
2022-08-16 11:14:04 -0300

    Criação de tela de login

--- social_challenge/Source/ViewController/LoginViewController.swift
+++ social_challenge/Source/ViewController/LoginViewController.swift
@@ -2,28 +2,92 @@
 //  LoginViewController.swift
 //  social_challenge
 //
-//  Created by Thiago Henrique on 16/08/22.
+//  Created by Marília de Sousa on 16/08/22.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
 
+    let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 40.0)
+        return label
+    }()
+    
+    let emailTextField: UITextField = {
+        let emailFild = UITextField ()
+        emailFild.placeholder = "E-mail"
+        emailFild.translatesAutoresizingMaskIntoConstraints = false
+        return emailFild
+    }()
+    
+    let passwordTextFiel: UITextField = {
+        let passwordFild = UITextField ()
+        passwordFild.placeholder = "Senha"
+        passwordFild.translatesAutoresizingMaskIntoConstraints = false
+        return passwordFild
+    }()
+    
+    lazy private var loginButton: UIButton = {
+        let button = UIButton ()
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 16.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(loginLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextFiel)
+        view.addSubview(loginButton)
+        
+        loadConstraint()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadConstraint () {
+        
+        let labelConstraint = [
+            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            loginLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150)
+        ]
+        
+        let emailTextFieldConstraint = [
+            emailTextField.leadingAnchor.constraint(equalTo: loginLabel.leadingAnchor),
+            emailTextField.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 50),
+            emailTextField.widthAnchor.constraint(equalToConstant: 300),
+            emailTextField.heightAnchor.constraint(equalToConstant: 45)
+        ]
+        
+        let passwordTextFielConstraint = [
+            passwordTextFiel.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextFiel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 50),
+            passwordTextFiel.widthAnchor.constraint(equalToConstant: 300),
+            passwordTextFiel.heightAnchor.constraint(equalToConstant: 45)
+        ]
+        
+        let loginButtonTextFieldConstraint = [
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.topAnchor.constraint(equalTo: passwordTextFiel.bottomAnchor, constant: 50),
+            loginButton.widthAnchor.constraint(equalToConstant: 150),
+            loginButton.heightAnchor.constraint(equalToConstant: 45)
+        ]
+        
+        
+        
+        NSLayoutConstraint.activate(labelConstraint)
+        NSLayoutConstraint.activate(emailTextFieldConstraint)
+        NSLayoutConstraint.activate(passwordTextFielConstraint)
+        NSLayoutConstraint.activate(loginButtonTextFieldConstraint)
+        
     }
-    */
-
+    
+    
 }
